f9fd6a5bda864b880e5c6a0ee1e00d0909430583
Shinichi Sato <hyotang.karakoma@gmail.com>
2016-09-06 09:07:48 +0900

    define: load-system

--- load-system/load-system.lisp
+++ load-system/load-system.lisp
@@ -3,3 +3,11 @@
     #:load-system
     ))
 (in-package :ql-tools.load-system)
+(named-readtables:in-readtable with-package:syntax)
+
+#@(:asdf #:*system-definition-search-functions*)
+
+(defun load-system(system)
+  (let((*system-definition-search-functions*(list* (make-searcher system)
+						   *system-definition-search-functions*)))
+    (asdf:load-system system)))
--- load-system/ql-tools.load-system.asd
+++ load-system/ql-tools.load-system.asd
@@ -1,4 +1,5 @@
 ; vim: ft=lisp et
 (in-package :asdf)
 (defsystem :ql-tools.load-system
+  :depends-on (:with-package)
   :components((:file "load-system")))
